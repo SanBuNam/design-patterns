606b76652e2111626ba89eac1fcb404f758f6118
David Lee <david.lee@amwestfunding.com>
2023-11-08 11:31:11 -0800

    Singleton pattern and prototype

---
+++ GOF-design-patterns/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOF_design_patterns
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Prototype Patter Demo***\n");
+            // Base or Original Copy
+            BasicCar nano_base = new Nano("Green Nano") { Price = 100000 };
+
+        }
+    }
+}
