ea18edb2b005c03d448632bd176a6e315123cd24
David Lee <david.lee@amwestfunding.com>
2023-11-29 10:45:34 -0800

    refactoring folder structure

---
+++ GOF-design-patterns/CreationalPatterns/AbstractFactory.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOF_design_patterns.CreationalPatterns
+{
+    public interface IDog
+    {
+        void Speak();
+        void Action();
+    }
+
+    public interface ITiger
+    {
+        void Speak();
+        void Action();
+    }
+
+    class WildDog : IDog
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Wild Dog says: Bow-Wow.");
+        }
+        public void Action()
+        {
+            Console.WriteLine("Wild Dogs prefer to roam freely in jungles.\n");
+        }
+    }
+
+    class WildTiger : ITiger
+    {
+        public void Speak() 
+        {
+            Console.WriteLine("Wild Tiger says: Halum.");
+        }
+        public void Action()
+        {
+            Console.WriteLine("Wild Tigers prefer hunting in jungles.\n");
+        }
+    }
+
+    class PetDog : IDog
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Pet Dog says: Bow-Wow.");
+        }
+        public void Action()
+        {
+            Console.WriteLine("Pet Dogs prefer to stay at home.\n");
+        }
+    }
+
+    class PetTiger : ITiger
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Pet Tiger says: Halum.");
+        }
+        public void Action()
+        {
+            Console.WriteLine("Pet Tigers play in an animal circus.\n");
+        }
+    }
+
+    public interface IAnimalFactoryAbstract
+    {
+        IDog GetDog();
+        ITiger GetTiger();
+    }
+
+    public class WildAnimalFactory : IAnimalFactoryAbstract
+    {
+        public IDog GetDog()
+        {
+            return new WildDog();
+        }
+        public ITiger GetTiger()
+        {
+            return new WildTiger();
+        }
+    }
+
+    public class PetAnimalFactory : IAnimalFactoryAbstract
+    {
+        public IDog GetDog()
+        {
+            return new PetDog();
+        }
+        public ITiger GetTiger()
+        {
+            return new PetTiger();
+        }
+    }
+
+    class AbstractFactoryProgram
+    {
+        static void AbstractFactoryMain(string[] args)
+        {
+            IAnimalFactoryAbstract wildAnimalFactory = new WildAnimalFactory();
+            IDog wildDog = wildAnimalFactory.GetDog();
+            wildDog.Speak();
+            wildDog.Action();
+
+            IAnimalFactoryAbstract petAnimalFactory = new PetAnimalFactory();
+            IDog petDog = petAnimalFactory.GetDog();
+            petDog.Speak();
+            petDog.Action();
+
+            ITiger wildTiger = wildAnimalFactory.GetTiger();
+            wildTiger.Speak();
+            wildTiger.Action();
+
+            ITiger petTiger = petAnimalFactory.GetTiger();
+            petTiger.Speak();
+            petTiger.Action();
+        }
+    }
+}
