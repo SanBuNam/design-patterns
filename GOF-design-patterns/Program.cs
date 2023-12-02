cfc08b76a5f19f0580ede3f7bef38a33f568ee3b
David Lee <david.lee@amwestfunding.com>
2023-12-01 17:08:43 -0800

    Proxy pattern refactor to Structural pattern

--- GOF-design-patterns/Program.cs
+++ GOF-design-patterns/Program.cs
@@ -6,45 +6,8 @@ using System.Threading.Tasks;
 
 namespace GOF_design_patterns
 {
-    // Abstract class subject
-    public abstract class Subject
-    {
-        public abstract void DoSomeWork();
-    }
-
-    // ConcreteSubject class
-    public class ConcreteSubject : Subject
-    {
-        public override void DoSomeWork()
-        {
-            Console.WriteLine("ConcreteSubject.DoSomeWork()");
-        }
-    }
-
-    // Proxy class
-    public class Proxy : Subject
-    {
-        Subject cs;
-        public override void DoSomeWork()
-        {
-            Console.WriteLine("Proxy call happening now...");
-            // Lazy initialization : We'll instantiate untill the method is called.
-            if (cs == null)
-            {
-                cs = new ConcreteSubject();
-            }
-            cs.DoSomeWork();
-        }
-    }
-
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("***Proxy Pattern Demo***\n");
-            Proxy px = new Proxy();
-            px.DoSomeWork();
-            Console.ReadKey();
-        }
+        
     }
 }
---
+++ GOF-design-patterns/StructuralPatterns/Proxy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOF_design_patterns.StructuralPatterns
+{
+    // Abstract class subject
+    public abstract class Subject
+    {
+        public abstract void DoSomeWork();
+    }
+
+    // ConcreteSubject class
+    public class ConcreteSubject : Subject
+    {
+        public override void DoSomeWork()
+        {
+            Console.WriteLine("ConcreteSubject.DoSomeWork()");
+        }
+    }
+
+    // Proxy class
+    public class Proxy : Subject
+    {
+        Subject cs;
+        public override void DoSomeWork()
+        {
+            Console.WriteLine("Proxy call happening now...");
+            // Lazy initialization : We'll instantiate untill the method is called.
+            if (cs == null)
+            {
+                cs = new ConcreteSubject();
+            }
+            cs.DoSomeWork();
+        }
+    }
+
+
+    class ProxyProgram
+    {
+        static void ProxyMain(string[] args)
+        {
+            Console.WriteLine("***Proxy Pattern Demo***\n");
+            Proxy px = new Proxy();
+            px.DoSomeWork();
+            Console.ReadKey();
+        }
+    }
+}
