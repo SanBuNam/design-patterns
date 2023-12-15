496e05d5442a22d2107c7e533e5726a08975e90c
David Lee <david.lee@amwestfunding.com>
2023-12-15 10:43:47 -0800

    composition pattern

--- GOF-design-patterns/Program.cs
+++ GOF-design-patterns/Program.cs
@@ -8,103 +8,106 @@ using GOF_design_patterns.StructuralPatterns;
 
 namespace GOF_design_patterns
 {
-    interface IRobot
+    interface IEmployee
     {
-        void Print();
+        void PrintStructures();
     }
-    // ConcreteFlyweight
-    class Robot : IRobot
+    class CompositeEmployee : IEmployee
     {
-        String robotType;
-        public String colorOfRobot;
-        public Robot(String robotType)
+        private string name;
+        private string dept;
+        // The container for child objects
+        private List<IEmployee> controls;
+        // constructor
+        public CompositeEmployee(string name, string dept)
         {
-            this.robotType = robotType;
+            this.name = name;
+            this.dept = dept;
+            controls = new List<IEmployee>();
         }
-        public void SetColor(String colorOfRobot)
+        public void Add(IEmployee e)
         {
-            this.colorOfRobot = colorOfRobot;
+            controls.Add(e);
         }
-        public void Print()
+        public void Remove(IEmployee e)
         {
-            Console.WriteLine("This is a" + robotType + "type robot with" + colorOfRobot + "color");
+            controls.Remove(e);
         }
-    }
-
-    // FlyweightFactory
-    class RobotFactory
-    {
-        Dictionary<string, IRobot> shapes = new Dictionary<string, IRobot>();
-        public int TotalObjectsCreated()
+        public void PrintStructures()
         {
-            return shapes.Count;
-        }
-
-        public IRobot GetRobotFromFactory(String robotType)
-        {
-            IRobot robotCategory = null;
-            if (shapes.ContainsKey(robotType))
+            Console.WriteLine("\t" + this.name + "works in" + this.dept);
+            foreach (IEmployee e in controls)
             {
-                robotCategory = shapes[robotType];
+                e.PrintStructures();
             }
-            else
-            {
-                switch(robotType)
-                {
-                    case "Small":
-                        robotCategory = new Robot("Small");
-                        shapes.Add("Small", robotCategory);
-                        break;
-                    case "Large":
-                        robotCategory = new Robot("Large");
-                        shapes.Add("Large", robotCategory);
-                        break;
-                    default:
-                        throw new Exception("Robot Factory can create only one each type robot");
-                }
-            }
-            return robotCategory;
         }
     }
 
-    // Flyweight pattern is in action
+    class Employee : IEmployee
+    {
+        private string name;
+        private string dept;
+        // constructor
+        public Employee(string name, string dept)
+        {
+            this.name = name;
+            this.dept = dept;
+        }
+        public void PrintStructures()
+        {
+            Console.WriteLine("\t\t" + this.name + "works in" + this.dept);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            RobotFactory myfactory = new RobotFactory();
-            Robot shape;
-            for (int i = 0; i < 3; i++)
-            {
-                shape = (Robot)myfactory.GetRobotFromFactory("Small");
-                Thread.Sleep(1000);
-                shape.SetColor(getRandomColor());
-                shape.Print();
-            }
-            for (int i = 0; i < 3; i++)
-            {
-                shape = (Robot)myfactory.GetRobotFromFactory("Large");
-                Thread.Sleep(1000);
-                shape.SetColor(getRandomColor());
-                shape.Print();
-            }
-            int NumOfDistinctRobots = myfactory.TotalObjectsCreated();
-            Console.WriteLine("\n Finally no of Distinct Robot objects created: " + NumOfDistinctRobots);
-            Console.ReadKey();
-        }
+            Console.WriteLine("***Composite Pattern Demo***");
+            // Principal of the college
+            CompositeEmployee Principal = new CompositeEmployee("Dr.S.Som(Principal)", "Planning-Supervising-Managing");
+            // The College has 2 Head of Departments-One from Mathematics, One from Computer Sc.
+            CompositeEmployee hodMaths = new CompositeEmployee("Mrs.S.Das(HOD-Maths)", "Maths");
+            CompositeEmployee hodCompSc = new CompositeEmployee("Mr. V.Sarcar(HOD-CSE)","Computer Sc.");
+            // 2 other teachers works in Mathematics department
+            Employee mathTeacher1 = new Employee("Math Teacher-1", "Maths");
+            Employee mathTeacher2 = new Employee("Math Teacher-2", "Maths");
+            // 3 other teachers works in Computer Sc. department
+            Employee cseTeacher1 = new Employee("CSE Teacher-1", "Computer Sc.");
+            Employee cseTeacher2 = new Employee("CSE Teacher-2", "Computer Sc.");
+            Employee cseTeacher3 = new Employee("CSE Teacher-3", "Computer Sc.");
+            // Teachers of Mathematics directly reports to HOD-Maths
+            hodMaths.Add(mathTeacher1);
+            hodMaths.Add(mathTeacher2);
+            // Teachers of Computer Sc. directly reports to HOD-CSE
+            hodCompSc.Add(cseTeacher1);
+            hodCompSc.Add(cseTeacher2);
+            hodCompSc.Add(cseTeacher3);
+            // Principal is on top of college
+            // HOD-Math and Comp. Sc directly reports to  him
+            Principal.Add(hodMaths);
+            Principal.Add(hodCompSc);
+            // Printing the leaf-nodes and branches in the same way.
+            // i.e. in each case, we are calling PrintStructures() method
+            Console.WriteLine("\n Testing the structure of a Principal object");
+            // Prints the complete structure
+            Principal.PrintStructures();
 
-        private static string getRandomColor()
-        {
-            Random r = new Random();
-            int random = r.Next(1000);
-            if (random % 2 == 0)
-            {
-                return "red";
-            }
-            else
-            {
-                return "green";
-            }
+            Console.WriteLine("\n Testing the structure of a HOD object:");
+            Console.WriteLine("Teachers working at Computer Science department:");
+            // Prints the details of Computer Sc, department
+            hodCompSc.PrintStructures();
+
+            // Leaf node
+            Console.WriteLine("\n Testing the structure of a leaf node:");
+            mathTeacher1.PrintStructures();
+
+            // Suppose, one computer teacher is leaving now from the organization
+            hodCompSc.Remove(cseTeacher2);
+            Console.WriteLine("\n After CSE Teacher-2 resigned, the organization has following members:");
+            Principal.PrintStructures();
+
+            Console.ReadKey();
         }
     }
 }
