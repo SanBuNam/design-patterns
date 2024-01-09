1287a5b0d5bb96119021d36297586f1c7e4fd7ed
David Lee <david.lee@amwestfunding.com>
2024-01-09 11:18:10 -0800

    Template Method

---
+++ GOF-design-patterns/BehavioralPatterns/TemplateMethod/BasicEngineering.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOF_design_patterns.BehavioralPatterns.TemplateMethod
+{
+    public abstract class BasicEngineering
+    {
+        public void Papers()
+        {
+            // Common Papers;
+            Math();
+            SoftSkills();
+            // Specialized Paper:
+            SpecialPaper();
+        }
+        private void Math()
+        {
+            Console.WriteLine("Mathematics");
+        }
+        private void SoftSkills()
+        {
+            Console.WriteLine("SoftSkills");
+        }
+        public abstract void SpecialPaper();
+    }
+}
