5480a26c5ad3c8ca52d42498cef1c54fd11aa135
David Lee <david.lee@amwestfunding.com>
2024-01-10 17:13:58 -0800

    Iterator pattern refactoring

---
+++ GOF-design-patterns/BehavioralPatterns/Iterator/IteratorPattern.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GOF_design_patterns.BehavioralPatterns.Aggregate;
+using GOF_design_patterns.BehavioralPatterns.Iterator;
+
+namespace GOF_design_patterns.BehavioralPatterns.Iterator
+{
+    // Iterator pattern
+    // Definition: Provide a way to access the elements of an aggregate object sequentially without exposing its underlying representation.
+    class IteratorPatternProgram
+    {
+        static void IteratorPatternMain(string[] args)
+        {
+            Console.WriteLine("***Iterator Pattern Demo***");
+            ISubjects ScienceSubjects = new Science();
+            ISubjects ArtsSubjects = new Arts();
+            IIterator IteratorForScience = ScienceSubjects.CreateIterator();
+            IIterator IteratorForArts = ArtsSubjects.CreateIterator();
+            Console.WriteLine("\nScience subjects :");
+            Print(IteratorForScience);
+            Console.WriteLine("\nArts subjects :");
+            Print(IteratorForArts);
+            Console.ReadLine();
+        }
+
+        private static void Print(IIterator iterator)
+        {
+            while (!iterator.IsDone())
+            {
+                Console.WriteLine(iterator.Next());
+            }
+        }
+    }
+}
--- GOF-design-patterns/Program.cs
+++ GOF-design-patterns/Program.cs
@@ -10,30 +10,11 @@ using GOF_design_patterns.BehavioralPatterns.Iterator;
 
 namespace GOF_design_patterns
 {
-    // Iterator pattern
-    // Definition: Provide a way to access the elements of an aggregate object sequentially without exposing its underlying representation.
-    class IteratorProgram
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***Iterator Pattern Demo***");
-            ISubjects ScienceSubjects = new Science();
-            ISubjects ArtsSubjects = new Arts();
-            IIterator IteratorForScience = ScienceSubjects.CreateIterator();
-            IIterator IteratorForArts = ArtsSubjects.CreateIterator();
-            Console.WriteLine("\nScience subjects :");
-            Print(IteratorForScience);
-            Console.WriteLine("\nArts subjects :");
-            Print(IteratorForArts);
-            Console.ReadLine();
-        }
-
-        private static void Print(IIterator iterator)
-        {
-            while (!iterator.IsDone())
-            {
-                Console.WriteLine(iterator.Next());
-            }
+           
         }
     }
 }
