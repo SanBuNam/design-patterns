8763d5c1ee1aa7b6e2bb9aa9999b742bc15b1cbe
David Lee <david.lee@amwestfunding.com>
2024-02-16 14:32:21 -0800

    NullObject pattern

---
+++ GOF-design-patterns/AdditionalPatterns/SimpleFactory.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOF_design_patterns.AdditionalPatterns
+{/*
+     Simple Factory Pattern
+     : Create an object without exposing the instantiation logic to the client.
+     Concept
+     : In object-oriented programming, a factory is such an object that can create other objects.
+       A factory can be invoked in many different ways but most often, it uses a method that can return objects with varying prototypes.
+       Any subroutine that can help us to create these new objects, can be considered as a factory.
+       Most importantly, it will help you to abstract the process of object creation from the consumers of the applications.
+     */
+    public interface IAnimalSimple
+    {
+        void Speak();
+        void Action();
+    }
+    public class DogSimple : IAnimalSimple
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Dog says: Bow-Wow.");
+        }
+        public void Action()
+        {
+            Console.WriteLine("Dogs prefer barking...");
+        }
+    }
+    public class TigerSimple : IAnimalSimple
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Tiger says: Halum.");
+        }
+        public void Action()
+        {
+            Console.WriteLine("Tigers prefer hunting...");
+        }
+    }
+
+    public abstract class ISimpleFactory
+    {
+        public abstract IAnimalSimple CreateAnimal();
+    }
+
+    public class SimpleFactory : ISimpleFactory
+    {
+        public override IAnimalSimple CreateAnimal()
+        {
+            IAnimalSimple intendedAnimal = null;
+            Console.WriteLine("Enter your choice(0 for Dog, 1 for Tiger)");
+            string b1 = Console.ReadLine();
+            int input;
+            if (int.TryParse(b1, out input))
+            {
+                Console.WriteLine("You have entered {0}", input);
+                switch (input)
+                {
+                    case 0:
+                        intendedAnimal = new DogSimple();
+                        break;
+                    case 1:
+                        intendedAnimal = new TigerSimple();
+                        break;
+                    default:
+                        Console.WriteLine("You must enter either 0 or 1");
+                        //We'll throw a runtime exception for any other choices.
+                        throw new ApplicationException(String.Format("Unknown Animal cannot be instantiated"));
+                }
+            }
+            return intendedAnimal;
+        }
+    }
+
+    //A client is intereted to get an animal who can speak and perform an action.
+    class SimpleFactoryProgram
+    {
+        static void SimpleFactoryMain(string[] args)
+        {
+            Console.WriteLine("*** Simple Factory Pattern Demo ***\n");
+            IAnimalSimple preferredType = null;
+            ISimpleFactory simpleFactory = new SimpleFactory();
+            #region The code region that will vary based on users preference
+            preferredType = simpleFactory.CreateAnimal();
+            #endregion
+            #region The codes that do not change frequently
+            preferredType.Speak();
+            preferredType.Action();
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
--- GOF-design-patterns/Program.cs
+++ GOF-design-patterns/Program.cs
@@ -11,91 +11,162 @@ using GOF_design_patterns.BehavioralPatterns.Iterator;
 namespace GOF_design_patterns
 {
     /*
-     Simple Factory Pattern
-     : Create an object without exposing the instantiation logic to the client.
-     Concept
-     : In object-oriented programming, a factory is such an object that can create other objects.
-       A factory can be invoked in many different ways but most often, it uses a method that can return objects with varying prototypes.
-       Any subroutine that can help us to create these new objects, can be considered as a factory.
-       Most importantly, it will help you to abstract the process of object creation from the consumers of the applications.
+     Null Object pattern
+     In object-oriented computer programming, a null object is an object with no referenced value or with defined neutral ('null') behavior.
+     The null object design pattern describes the uses of such objects and their behavior (or lack thereof).
+     
+     Concept:
+     The pattern can implement a "do-nothing" relationship, or it can provide a default behavior when an application encounters a null object instead of a real object.
+     In simple words, the core aim is to make a better solution by avoiding a "null objects check" or "null collaborations check" through if blocks.
+     To explain the concept better, I will explain the problems associated with the following program (which is basically a faulty program), 
+     analyze the probable solutions, and ultimately, implement the concept of this design pattern.
      */
-    public interface IAnimalSimple
-    {
-        void Speak();
-        void Action();
-    }
-    public class DogSimple : IAnimalSimple
-    {
-        public void Speak()
-        {
-            Console.WriteLine("Dog says: Bow-Wow.");
-        }
-        public void Action()
-        {
-            Console.WriteLine("Dogs prefer barking...");
-        }
-    }
-    public class TigerSimple : IAnimalSimple
-    {
-        public void Speak()
-        {
-            Console.WriteLine("Tiger says: Halum.");
-        }
-        public void Action()
-        {
-            Console.WriteLine("Tigers prefer hunting...");
-        }
-    }
+    
+    //Faulty Program
+    //interface IVehicle
+    //{
+    //    void Travel();
+    //}
+    //class Bus : IVehicle
+    //{
+    //    public static int busCount = 0;
+    //    public Bus()
+    //    {
+    //        busCount++;
+    //    }
+    //    public void Travel()
+    //    {
+    //        Console.WriteLine("Let us travel with Bus");
+    //    }
+    //}
+    //class Train : IVehicle
+    //{
+    //    public static int trainCount = 0;
+    //    public Train()
+    //    {
+    //        trainCount++;
+    //    }
+    //    public void Travel()
+    //    {
+    //        Console.WriteLine("Let us travel with Train");
+    //    }
+    //}
 
-    public abstract class ISimpleFactory
-    {
-        public abstract IAnimalSimple CreateAnimal();
-    }
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("*** Null Object Pattern ***\n");
+    //        string input = String.Empty;
+    //        int totalObjects = 0;
+    //        while (true)
+    //        {
+    //            Console.WriteLine("Enter your choice(Type 'a' for Bus, 'b' for Train) ");
+    //            input = Console.ReadLine();
+    //            IVehicle vehicle = null;
+    //            switch (input)
+    //            {
+    //                case "a":
+    //                    vehicle = new Bus();
+    //                    break;
+    //                case "b":
+    //                    vehicle = new Train();
+    //                    break;
+    //            }
+    //            totalObjects = Bus.busCount + Train.trainCount;
+    //            vehicle.Travel();
+    //            Console.WriteLine("Total objects created in the system ={0}", totalObjects);
+    //        }
+    //    }
+    //}
 
-    public class SimpleFactory: ISimpleFactory
+
+    // Applying the NullObject pattern now
+    interface IVehicle
     {
-        public override IAnimalSimple CreateAnimal()
+        void Travel();
+    }
+    class Bus : IVehicle
+    {
+        public static int busCount = 0;
+        public Bus()
         {
-            IAnimalSimple intendedAnimal = null;
-            Console.WriteLine("Enter your choice(0 for Dog, 1 for Tiger)");
-            string b1 = Console.ReadLine();
-            int input;
-            if (int.TryParse(b1, out input))
+            busCount++;
+        }
+        public void Travel()
+        {
+            Console.WriteLine("Let us travel with Bus");
+        }
+    }
+    class Train : IVehicle
+    {
+        public static int trainCount = 0;
+        public Train()
+        {
+            trainCount++;
+        }
+        public void Travel()
+        {
+            Console.WriteLine("Let us travel with Train");
+        }
+    }
+    class NullVehicle : IVehicle
+    {
+        private static readonly NullVehicle instance = new NullVehicle();
+        public static int nullVehicleCount = 0;
+        public static NullVehicle Instance
+        {
+            get
             {
-                Console.WriteLine("You have entered {0}", input);
-                switch (input)
-                {
-                    case 0:
-                        intendedAnimal = new DogSimple();
-                        break;
-                    case 1:
-                        intendedAnimal = new TigerSimple();
-                        break;
-                    default:
-                        Console.WriteLine("You must enter either 0 or 1");
-                        //We'll throw a runtime exception for any other choices.
-                        throw new ApplicationException(String.Format("Unknown Animal cannot be instantiated"));
-                }   
+                //Console.WriteLine("We already have an instance now Use it.");
+                return instance;
             }
-            return intendedAnimal;
+        }
+        public void Travel()
+        {
+            //Do Nothing
         }
     }
 
-    //A client is intereted to get an animal who can speak and perform an action.
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] vs)
         {
-            Console.WriteLine("*** Simple Factory Pattern Demo ***\n");
-            IAnimalSimple preferredType = null;
-            ISimpleFactory simpleFactory = new SimpleFactory();
-            #region The code region that will vary based on users preference
-            preferredType = simpleFactory.CreateAnimal();
-            #endregion
-            #region The codes that do not change frequently
-            preferredType.Speak();
-            preferredType.Action();
-            #endregion
+            Console.WriteLine("*** Null Object Pattern ***\n");
+            string input = String.Empty;
+            int totalObjects = 0;
+            while (input != "exit")
+            {
+                Console.WriteLine("Enter your choice( Type 'a' for Bus, 'b' for Train.Type 'exit' to quit)");
+                input = Console.ReadLine();
+                IVehicle vehicle = null;
+                switch (input)
+                {
+                    case "a":
+                        vehicle = new Bus();
+                        break;
+                    case "b":
+                        vehicle = new Train();
+                        break;
+                    case "exist":
+                        Console.WriteLine("Closing the application");
+                        vehicle = NullVehicle.Instance;
+                        break;
+                    default:
+                        vehicle = NullVehicle.Instance;
+                        if (input == "exit")
+                        {
+                            Console.WriteLine("Closing the application. Press Enter at end.");
+                        }
+                        break;
+                }
+                totalObjects = Bus.busCount + Train.trainCount + NullVehicle.nullVehicleCount;
+                //ride the vehicle
+                //if (vehicle != null)
+                //    vehicle.Travel();
+                //}
+                Console.WriteLine("Total objects created in the system ={0}", totalObjects);
+            }
             Console.ReadKey();
         }
     }
